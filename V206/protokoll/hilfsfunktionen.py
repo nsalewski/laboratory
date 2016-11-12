1eeeee31615c4f7ab517ac61d294c094f9306ff5
Niko Salewski <niko.salewski@tu-dortmund.de>
2016-11-12 22:40:58 +0100

    corrected hilfsfunktionen.py

--- V206/protokoll/hilfsfunktionen.py
+++ V206/protokoll/hilfsfunktionen.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy.optimize import curve_fit
 from astropy.io import ascii
 t,T1,T2,pb,pa,leistung =np.genfromtxt("daten.txt", unpack=True)
 t=t*60
@@ -9,14 +9,12 @@ pb=pb+1
 pa=pa+1
 def f3(t,a,alpha,b,c):
         return(((a*t**alpha)/(1+b*t**alpha)+c))
-params1, covariance1 = curve_fit(f3, t, T1,bounds=([-np.inf,1,-np.inf, -np.inf],[np.inf,2,np.inf, np.inf]))
-params2, covariance2 = curve_fit(f3, t, T2,bounds=([-np.inf,1,-np.inf, -np.inf],[np.inf,2,np.inf, np.inf]))
-ascii.write([params1,params2,errors1,errors2], 'hilfsdateien/errparams.dat', format='latex')
-
-
+params1, covariance1 = curve_fit(f3, t, T1,bounds=([-np.inf,1,-np.inf, -np.inf],[np.inf,2,np.inf, np.inf] ))
+params2, covariance2 = curve_fit(f3, t, T2,bounds=([-np.inf,1,-np.inf, -np.inf],[np.inf,2,np.inf, np.inf] ))
 
 errors1 = np.sqrt(np.diag(covariance1))
 errors2 = np.sqrt(np.diag(covariance2))
 ascii.write([t, T1, T2, pb, pa, leistung], 'hilfsdateien/values.dat', format='latex')
 
 print(errors1)
+print(errors2)
