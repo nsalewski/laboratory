c3b00d40f918b00e5cb0388a26d0ff49d1ee4936
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2016-11-12 12:42:04 +0100

    some formatting still to do. a and b included in main.pdf

--- V206/protokoll/hilfsfunktionen.py
+++ V206/protokoll/hilfsfunktionen.py
@@ -7,4 +7,14 @@ T1=T1+273.15
 T2=T2+273.15
 pb=pb+1
 pa=pa+1
-ascii.write([t, T1, T2, pb, pa, leistung], 'values.dat', format='latex')
+def f3(t,a,alpha,b,c):
+        return(((a*t**alpha)/(1+b*t**alpha)+c))
+params1, covariance1 = curve_fit(f3, t, T1,bounds=([-np.inf,1,-np.inf, -np.inf],[np.inf,2,np.inf, np.inf]))
+params2, covariance2 = curve_fit(f3, t, T2,bounds=([-np.inf,1,-np.inf, -np.inf],[np.inf,2,np.inf, np.inf]))
+ascii.write([params1,params2,errors1,errors2], 'hilfsdateien/errparams.dat', format='latex')
+
+
+
+errors1 = np.sqrt(np.diag(covariance1))
+errors2 = np.sqrt(np.diag(covariance2))
+ascii.write([t, T1, T2, pb, pa, leistung], 'hilfsdateien/values.dat', format='latex')
--- V206/protokoll/plot.py
+++ V206/protokoll/plot.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+from astropy.io import ascii
 
 #aufgabe a: Messdaten plotten
 t,T1,T2,pb,pa,leistung =np.genfromtxt("daten.txt", unpack=True)
 t=t*60
-
-plt.plot(t, T1, 'o', label='Messdaten T1 (warmes Reservoir)')
-plt.plot(t, T2, 'o', label= 'Messdaten T2 (kaltes Reservoir')
+T1=T1+273.15
+T2=T2+273.15
+pb=pb+1
+pa=pa+1
+plt.plot(t, T1, 'o', label='T1')
+plt.plot(t, T2, 'o', label= 'T2')
 
 #Aufgabe b: Curvefit
 #f1+2
@@ -26,15 +30,21 @@ def f3(t,a,alpha,b,c):
         return(((a*t**alpha)/(1+b*t**alpha)+c))
 params1, covariance1 = curve_fit(f3, t, T1,bounds=([-np.inf,1,-np.inf, -np.inf],[np.inf,2,np.inf, np.inf]))
 params2, covariance2 = curve_fit(f3, t, T2,bounds=([-np.inf,1,-np.inf, -np.inf],[np.inf,2,np.inf, np.inf]))
-print(params1)
-print(params2)
-
-
+#hilfsfunktionen
 
 errors1 = np.sqrt(np.diag(covariance1))
 errors2 = np.sqrt(np.diag(covariance2))
-
+ascii.write([t, T1, T2, pb, pa, leistung], 'hilfsdateien/values.dat', format='latex')
+ascii.write([params1,errors1], 'hilfsdateien/paramserr1.dat', format='latex')
+ascii.write([params2,errors2], 'hilfsdateien/paramserr2.dat', format='latex')
 plt.plot(t,f3(t,*params1), label ="Fit für T1")
 plt.plot(t,f3(t,*params2), label ="Fit für T2")
+plt.grid()
+plt.legend(loc='best')
+plt.xlabel(r'$\frac{t}{s}$')
+plt.ylabel(r'$Temperaturen \cdot\frac{1}{K}$')
+plt.xlim(0, t[len(t)-1]+80)
+plt.tight_layout()
+
 
 plt.savefig("build/plot.pdf")
