75cf7f4a795bba61fa40c2b6383879416b1ebf1e
Niko Salewski <niko.salewski@tu-dortmund.de>
2016-11-12 23:25:44 +0100

    fehlerfortpflanzung in plot.py

--- V206/protokoll/plot.py
+++ V206/protokoll/plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 from astropy.io import ascii
+import sympy
 
 #aufgabe a: Messdaten plotten
 t,T1,T2,pb,pa,leistung =np.genfromtxt("daten.txt", unpack=True)
@@ -48,3 +49,26 @@ plt.tight_layout()
 
 
 plt.savefig("build/plot.pdf")
+
+
+def error(f, err_vars=None):
+    from sympy import Symbol, latex
+    s = 0
+    latex_names = dict()
+
+    if err_var == None:
+        err_vars == f.free_symbols
+
+    for v in err_vars:
+        err = Symbol('latex_std' + v.name)
+        s += f.diff(v)**2 * err**2
+        latex_names[err] = '\\sigma_{ ' + latex(v) + '}'
+
+    return latex(sympy.sqrt(s), symbol_names = latex_names)
+
+
+t, A, B, a = sympy.var('t A B a')
+
+f = (a * A * t^(a-1)) / (1 + B * t^a)^2
+print(f)
+print(error(f))
