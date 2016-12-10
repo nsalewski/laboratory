c1e51758ce7c4671f80fdfa34e13b98d4d3f981c
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2016-12-10 21:32:15 +0100

    finished a and b, started discussion

--- V353/protokoll/amplitude.py
+++ V353/protokoll/amplitude.py
@@ -4,22 +4,26 @@ from scipy.optimize import curve_fit
 from astropy.io import ascii
 
 
+w, urc, ug, phi = np.genfromtxt("Messdaten/b_c.txt", unpack=True)
+unull = 7280
+urc = urc * 1000
 
-w, urc,ug,phi=np.genfromtxt("Messdaten/b_c.txt", unpack=True)
-unull=728
-urc=urc*100
+
 def f(x, a):
-    return (unull/(np.sqrt(1+(x**2)*(a**2))))
-params, covariance = curve_fit(f,w,urc)
+    return (unull / (np.sqrt(1 + (x**2) * (a**2))))
+params, covariance = curve_fit(f, w, urc)
 errors = np.sqrt(np.diag(covariance))
-print('a = (RC) =', np.sqrt(params[0]), '±', np.sqrt(errors[0]))
+print('a = (RC) =', np.sqrt(params[0]), '±',
+      errors[0] / 2 * np.sqrt(params[0]))
 print(params)
-ascii.write([urc,w ], 'Messdaten/b.tex', format="latex")
+ascii.write([urc, w], 'Messdaten/b.tex', format="latex")
 
-temp=(f(w,*params))#used temp, bc without temp there was really freaky and wrong behaviour in matplotlib
+# used temp, bc without temp there was really freaky and wrong behaviour
+# in matplotlib
+temp = (f(w, *params))
 plt.plot(w, urc, 'rx', label="Messwerte")
 plt.plot(w, temp, 'b-', label='Ausgleichsgerade')
-plt.xlim(4.24,10000)
+plt.xlim(4.24, 10000)
 plt.xlabel("$\omega$ / $\si{\Hz}$")
 plt.ylabel(r"$A(\omega)$/ $\si{\milli \volt}$")
 plt.xscale('log')
--- V353/protokoll/plot.py
+++ V353/protokoll/plot.py
@@ -9,21 +9,24 @@ from astropy.io import ascii
 #from sympy import *
 #from pylab import *
 
-uc,t=np.genfromtxt("Messdaten/a.txt", unpack=True)
-unull=7.28
-t=t/1000
-ascii.write([uc, np.log(uc/unull),t ], 'Messdaten/a.tex', format="latex", names=[r'$U_\\text{C}$/$\\si{\\volt}$', '$\\ln{(\\frac{U_\\text{C}}{U_\\text{0}})}$', "$t$ /$10^{-6}\\si{\\second}$" ])
+uc, t = np.genfromtxt("Messdaten/a.txt", unpack=True)
+unull = 7.28
+t = t / 1000
+ascii.write([uc, np.log(uc / unull), t], 'Messdaten/a.tex', format="latex",
+            names=[r'$U_\\text{C}$/$\\si{\\volt}$', '$\\ln{(\\frac{U_\\text{C}}{U_\\text{0}})}$', "$t$ /$10^{-6}\\si{\\second}$"])
+
+
 def f(x, a, b):
-    return (-1/a) * x + b
-params, covariance = curve_fit(f,t, np.log(uc/unull))
+    return (-1 / a) * x + b
+params, covariance = curve_fit(f, t, np.log(uc / unull))
 errors = np.sqrt(np.diag(covariance))
 print('a = ', params[0], '±', errors[0])
 print(params)
-m=np.linspace(-0.1,2)
-plt.plot(t, np.log(uc/unull), 'rx', label="Messwerte")
+m = np.linspace(-0.1, 2)
+plt.plot(t, np.log(uc / unull), 'rx', label="Messwerte")
 plt.plot(m, f(m, *params), 'b-', label='Ausgleichsgerade')
 #plt.xlabel(r'$t$ /$10^{-6}\\si{\\second}$')
-#plt.ylabel(r'$\\ln{(\\frac{U_\\text{C}}{U_\\text{0}})}$')
+# plt.ylabel(r'$\\ln{(\\frac{U_\\text{C}}{U_\\text{0}})}$')
 plt.legend(loc='best')
-plt.xlim([0,2])
+plt.xlim([0, 2])
 plt.savefig("build/test.pdf")
