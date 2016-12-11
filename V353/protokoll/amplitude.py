fe0d311223f8158487428a86d7b6d52b6c3e87be
Niko Salewski <niko.salewski@tu-dortmund.de>
2016-12-11 16:13:55 +0100

    c) done

--- V353/protokoll/amplitude.py
+++ V353/protokoll/amplitude.py
@@ -5,17 +5,15 @@ from astropy.io import ascii
 
 
 w, urc, ug, phi = np.genfromtxt("Messdaten/b_c.txt", unpack=True)
-unull = 7280
-urc = urc * 1000
+unull = 7280 / 1000
+urc = urc
 
 
 def f(x, a):
     return (unull / (np.sqrt(1 + (x**2) * (a**2))))
 params, covariance = curve_fit(f, w, urc)
 errors = np.sqrt(np.diag(covariance))
-print('a = (RC) =', params[0], '±',
-      errors[0])
-print(params)
+print('a = (RC) =', params[0], '±', errors[0])
 ascii.write([urc, w], 'Messdaten/b.tex', format="latex")
 
 # used temp, bc without temp there was really freaky and wrong behaviour
--- V353/protokoll/phase.py
+++ V353/protokoll/phase.py
@@ -5,7 +5,7 @@ from astropy.io import ascii
 
 
 w, urc, ug, a = np.genfromtxt("Messdaten/b_c.txt", unpack=True)
-
+unull = 7.28
 a = a / 1000
 b = 1 / w
 phi = 2 * np.pi * a / b
@@ -13,18 +13,18 @@ phi = 2 * np.pi * a / b
 def f(w, c):
     return np.arctan(-w * c)
 
-params, covariance = curve_fit(f, w, phi)
+params, covariance = curve_fit(f, w, phi) # bounds = ([0], [np.inf]))
 errors = np.sqrt(np.diag(covariance))
 print('c =', params[0], '±', errors[0])
 print(params)
 ascii.write([w, a, b, phi], 'Messdaten/c.tex', format="latex")
-
+ascii.write([phi, urc/unull], 'Messdaten/pol.tex', format = 'latex')
 # used temp, bc without temp there was really freaky and wrong behaviour
 # in matplotlib
 m=np.logspace(0.01,4,)
 temp = (f(m, *params))
 plt.plot(w, phi, 'rx', label="Messwerte")
-plt.plot(m, temp, 'b-', label='Ausgleichsgerade')
+plt.plot(m, temp, 'b-', label='Ausgleichskurve')
 plt.xlim(4.24, 10000)
 plt.xlabel("$\omega$ / $\si{\Hz}$")
 plt.ylabel(r"$\phi(\omega)$/ $\si{\radian}$")
@@ -32,3 +32,10 @@ plt.xscale('log')
 plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig("build/phase.pdf")
+
+plt.clf()
+phi_ = np.linspace(0, np.pi/2, 1000)
+v = -np.tan(phi_) / (3.67 * 10**(-3))
+plt.polar(phi, urc/unull, 'rx', label = 'Messwerte')
+plt.polar(phi_, -np.sin(phi_) / (v * 3.67 * 10 ** (-3)), 'b-', label = 'Theoriekurve')
+plt.savefig('polaar.pdf')
