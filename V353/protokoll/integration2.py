029215f8c69f2cd07516b546215ebd32c11c2b6b
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2016-12-12 12:17:02 +0100

    updated plot legend

--- V353/protokoll/integration2.py
+++ V353/protokoll/integration2.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.optimize import curve_fit
 x = np.linspace(-10, 10, 300)
 plt.plot(x, np.sin(x), 'r-', label="Sinus")
-plt.plot(x, -0.7 * np.cos(x), 'b-', label='Cosinus')
+plt.plot(x, -0.7 * np.cos(x), 'b-', label='- Cosinus')
 plt.xlim(-10, 10)
 plt.xlabel(r"$x$")
 plt.ylabel(r"$f(x)$")
