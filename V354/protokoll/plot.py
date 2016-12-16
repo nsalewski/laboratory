5d1ba8a49d18f989db771d96c28d0ca08d7feeee
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2016-12-17 00:12:40 +0100

    edited pics, just keepin' it up to date

--- V354/protokoll/plot.py
+++ V354/protokoll/plot.py
@@ -2,26 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 from astropy.io import ascii
-#import sympy
-#from uncertainties import ufloat
-#import uncertainties.unumpy as unp
-#from sympy import Symbol, latex
-#from sympy import *
-#from pylab import *
+import sympy
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+from sympy import Symbol, latex
+from sympy import *
+from pylab import *
 uc, t = np.genfromtxt("Messdaten/a_and_b.txt", unpack=True)
 
 ascii.write([uc, t], 'Messdaten/tab_a.tex', format="latex",
             names=[r'$U_\\text{C}$/$\\si{\\volt}$', "$t$ /$10^{-6}\\si{\\second}$"])
 
 def f(x, a, b):
-    return a*np.exp(-2*np.pi*b*t)
+    return a*np.exp(-2*np.pi*b*t*(1/1000000))
 params, covariance = curve_fit(f, t, uc)
 errors = np.sqrt(np.diag(covariance))
+print(errors)
 print('A_0 = ', params[0], '±', errors[0])
 print('mü = ', params[1], '±', errors[1])
-m=np.linspace(-10,500,150)
 plt.plot(t, uc, 'rx', label="Messwerte")
-plt.plot(t, f(t, *params), 'b-', label='Ausgleichsgerade')
+plt.plot(t, f(t, *params), 'b-', label='Ausgleichsgerade') #didnt work with linspace for me, dunno why.
 plt.xlabel(r'$t$ /$10^{-6}\si{\second}$')
 plt.ylabel(r'$U_\text{C}$/$\si{\volt}$')
 plt.legend(loc='best')
