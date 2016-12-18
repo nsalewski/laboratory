aa65f16296e790a95e2fe2b8cd33637c2a2aad30
Niko Salewski <niko.salewski@tu-dortmund.de>
2016-12-18 16:31:10 +0100

    auf auf zur show

--- V354/protokoll/taskd.py
+++ V354/protokoll/taskd.py
@@ -13,10 +13,13 @@ C = 2.098 / 10**9
 def f(w):
     return np.arctan(-(w * R * C) / (1 - L * C * w**2))
 
-ascii.write([w, a, b, phi], 'Messdaten/d.tex', format="latex")
+ascii.write([w, a, b*10**6, phi], 'Messdaten/d.tex', format="latex")
 
 #m = np.logspace(0.01, 4)
 #temp = (f(m, *params))
+x1=[30.5, 30.5]
+x2=[37.5, 37.5]
+y1=[0,3.5]
 
 plt.figure(0)
 plt.plot(w, phi, 'rx', label="Messwerte")
@@ -31,6 +34,8 @@ plt.savefig("build/taskd.pdf")
 
 plt.figure(1)
 plt.plot(w, phi, 'rx', label="Messwerte")
+plt.plot(x1,y1,"g--", label=r"$ \omega_\mathrm{+}$ und $\omega_\mathrm{-}$")
+plt.plot(x2,y1,'g--')
 plt.axhline(y = np.pi / 4)
 plt.axhline(y = np.pi / 2)
 plt.axhline(y = 3 * np.pi / 4)
@@ -38,6 +43,8 @@ plt.xlim(10, 40)
 plt.xlabel("$\omega$ / $\si{\Hz}$")
 plt.ylabel(r"$\phi(\omega)$/ $\si{\radian}$")
 plt.legend(loc='best')
+plt.grid()
 plt.tight_layout()
 plt.savefig("build/taskdlinear.pdf")
+
 
