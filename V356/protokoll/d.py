54e84a79aa227436bd6da4b2c75a9a24a8b0560d
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-01-03 14:41:32 +0100

    finished c and d

---
+++ V356/protokoll/d.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from astropy.io import ascii
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+n1,u1=np.genfromtxt("Messdaten/c_1.txt",unpack=True)
+n2,u2=np.genfromtxt("Messdaten/c_2.txt", unpack=True)
+n3,u3=np.genfromtxt("Messdaten/d.txt", unpack=True)
+ascii.write([n1, u1,u2], 'Messdaten/tab_d.tex', format="latex",
+            names=['Nummer des Kettenglieds',r'$U$/$\\si{\\volt}$ bei 1.Eigenschwingung', "$U$/$\\si{\\volt}$ bei 2.Eigenschwingung"])
+plt.plot(n1, u1, 'ro--', label="Messdaten 1. Eigenschwingung (7.5 \,\si{\kilo\Hz})")
+plt.ylabel(r'Spannung $U$/$\si{\volt}$')
+plt.xlabel(r'Nr. des $LC$-Kettenglieds')
+plt.xlim(-1,14)
+plt.ylim(0,1.4)
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('Bilder/d.pdf')
+plt.clf()
+
+plt.plot(n2, u2, 'ro--', label='Messdaten 2. Eigenschwingung (14.7\,\si{\kilo\Hz})')
+plt.ylabel(r'Spannung $U$/$\si{\volt}$')
+plt.xlabel(r'Nr. des $LC$-Kettenglieds')
+plt.legend(loc='best')
+plt.xlim(-1,14)
+plt.tight_layout()
+plt.savefig('Bilder/d2.pdf')
+plt.clf()
+
+ascii.write([n3,u3],'Messdaten/tab_dlame.tex', format="latex",
+            names=['Nummer des Kettenglieds',r'$U$/$\\si{\\volt}$ bei 1.Eigenschwingung und Abschlusswiderstand $R=Z$'])
+plt.plot(n3, u3, 'ro--', label='Messdaten zur 1. Eigenschwingung (7.5\,\si{\kilo\Hz})')
+plt.ylabel(r'Spannung $U$/$\si{\volt}$')
+plt.xlabel(r'Nr. des $LC$-Kettenglieds')
+plt.legend(loc='best')
+plt.xlim(-1,14)
+plt.tight_layout()
+plt.savefig('Bilder/d3.pdf')
--- V356/protokoll/plot.py
+++ V356/protokoll/plot.py
@@ -1,2 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
+from astropy.io import ascii
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+n,w=np.genfromtxt("Messdaten/c_1.txt",unpack=True)
+n=n+1
+theta=(n*np.pi)/14
+kreisfrequenz=w*2*np.pi
+phase=kreisfrequenz/theta
+L=1.217*1/10**4
+C=20.13*1/10**9
+def theorie(f):
+    return ((2*np.pi*f)/(np.arccos(1-((1/2)*L*C*(2*np.pi*f)**2))))
+
+ascii.write([n, theta,w,w*2*np.pi,phase], 'Messdaten/tab_c.tex', format="latex",
+            names=['n','theta','frequenz','kreis','Phase'])
+plt.plot(w, phase, 'rx', label="Messwerte")
+plt.plot(w, theorie(w), 'b-', label="Theoriekurve")
+
+plt.ylabel(r'Phasengeschwindigkeit')
+plt.xlabel(r'Frequenz')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('Bilder/Phasengeschwindigkeit.pdf')
