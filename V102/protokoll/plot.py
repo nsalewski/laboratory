b06eb226913b600a73992c848a2f27892c40c073
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-01-11 00:29:25 +0100

    added V102, corrected V354-I believe, I will beautify table in discussion

---
+++ V102/protokoll/plot.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from astropy.io import ascii
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
--- V356/protokoll/a.py
+++ V356/protokoll/a.py
@@ -5,21 +5,19 @@ from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
 
-n,f=np.genfromtxt("Messdaten/a_2.txt",unpack=True)
-n=n*4
+n, f = np.genfromtxt("Messdaten/a_2.txt", unpack=True)
+n = n * 4
 
-ascii.write([n,f], 'Messdaten/tab_a1.tex', format="latex",
-            names=['n','frequenz'])
+ascii.write([n, f], 'Messdaten/tab_a1.tex', format="latex",
+            names=['n', 'frequenz'])
+
 
 def w(x, a, b):
-    return a*np.exp(b*x)
+    return a * np.exp(b * x)
 
-params, covariance = curve_fit(w,n,f)
+params, covariance = curve_fit(w, n, f)
 
 errors = np.sqrt(np.diag(covariance))
 
 print('a = ', params[0], '+-', errors[0])
 print('b = ', params[1], '+-', errors[1])
-
-
-
