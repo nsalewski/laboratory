8a730d83c0951c4bcd4b2e688d0dbdc5e0db5550
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-01-15 17:02:32 +0100

    got milliTesla for B-Feld

--- V102/protokoll/be.py
+++ V102/protokoll/be.py
@@ -10,4 +10,16 @@ n, Te = np.genfromtxt("Messdaten/c.txt", unpack=True)
 ascii.write([n, Te], 'Messdaten/c.tex', format="latex",
             names=["Anzahl", "Periode"])
 
-print('Mittelwert:', np.mean(Te), ' und Fehler des Mittelwerts: ', np.std(Te, ddof=1)/np.sqrt(len(Te)))
+print('Mittelwert:', np.mean(Te), ' und Fehler des Mittelwerts: ',
+      np.std(Te, ddof=1) / np.sqrt(len(Te)))
+
+
+T_mittel = ufloat(np.mean(Te), np.std(Te, ddof=1) / np.sqrt(len(Te)))
+Theta_ges = ufloat(1.3422, 0.0006) / 10000
+magn_moment = ufloat(0.0339, 0.0004)
+G = ufloat(90, 5) / 10**(9)
+R = ufloat(86, 1.1) / 10**(6)
+L = 58.5 / 100
+B = ((((np.pi * 2) / T_mittel)**2 * Theta_ges) /
+     (magn_moment)) - ((np.pi * G * R**4) / (2 * L * magn_moment))
+print("Erdmagnetfeld= ", B)
