d1c39172317ff596e172a3f198ec4633dfa1d1ca
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-01-21 02:19:34 +0100

    finished auswertung, some formatting still to do. included longtables

--- V103/protokoll/beidi.py
+++ V103/protokoll/beidi.py
@@ -4,7 +4,7 @@ from scipy.optimize import curve_fit
 from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
-g = 9.81  # scipy einfügen
+g = ufloat(9.811899, 0.000041)
 
 x, d0, d = np.genfromtxt("Messdaten/c.txt", unpack=True)
 D = d - d0
@@ -17,7 +17,7 @@ ascii.write([x, d0, d, D], 'Messdaten/beidseitig.tex', format="latex",
             names=["messpunkt x ", "D0", "Dlast", "D"])
 
 m_alu, d_alu, dx_alu = np.genfromtxt("Messdaten/a.txt", unpack=True)
-L_stab_alueingespannt = 0.53
+L_stab_alueingespannt = 0.56
 mlast_alu = 4720.1
 L_stab_alu = 60
 d_stab_alu = 0.01
@@ -30,9 +30,10 @@ print("Dichte Alu Literatur", dichte_lit_alu)
 
 
 x = x / 100
-x1 = x1/100
-x2 = x2/100
-x_alu_fit = ((3*L_stab_alueingespannt**2) * x1- 4*x1**3)
+x1 = x1 / 100
+x2 = x2 / 100
+x_alu_fit = ((3 * L_stab_alueingespannt**2) * x1 - 4 * x1**3)
+
 
 def Y1(x, a):
     return a * x
@@ -55,16 +56,18 @@ I_alu = d_stab_alu**4 / 12
 
 E_alu = F_alu / (48 * a_alu * I_alu)
 print("E alu=", E_alu)
-##################################################################################
+##########################################################################
 
-def Y2(x,A):
-    return A*x
 
-x_alufit = 4*x2**3 - 12 * L_stab_alueingespannt * x2**2 + 9*L_stab_alueingespannt**2*x2 - L_stab_alueingespannt**3
+def Y2(x, A):
+    return A * x
+
+x_alufit = 4 * x2**3 - 12 * L_stab_alueingespannt * x2**2 + \
+    9 * L_stab_alueingespannt**2 * x2 - L_stab_alueingespannt**3
 plt.clf()
 params, covariance = curve_fit(Y2, x_alufit, D2)
 errors = np.sqrt(np.diag(covariance))
-print("params", *params, "fehler" , *errors)
+print("params", *params, "fehler", *errors)
 plt.plot(x_alufit, D2, 'rx', label="Messwerte")
 plt.plot(x_alufit, Y2(x_alufit, *params), 'b-', label="Regressionsgrade")
 plt.xlabel(
--- V103/protokoll/plot.py
+++ V103/protokoll/plot.py
@@ -4,12 +4,12 @@ from scipy.optimize import curve_fit
 from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
-g = 9.81  # scipy einfügen
-x_linspace = np.linspace(0, 53)
+g = ufloat(9.811899, 0.000041)
+x_linspace = np.linspace(0, 50) / 100
 
 
-def D(x, a, b):
-    return a * x + b
+def D(x, a):
+    return a * x
 
 
 m_messing, d_messing, dx_messing = np.genfromtxt(
@@ -29,29 +29,37 @@ dichte_lit_messing = 8.400  # jeweils nach engineers toolbox
 print("Dichte Messing Literatur", dichte_lit_messing)
 I_messing = np.pi * r_stab_messing**4 / 4
 F_messing = mlast_messing * g
+print("I messing=", I_messing)
 
 x_messing = x_messing / 100
 x_messing_fit = (L_stab_messing_eingespannt *
                  x_messing**2 - ((x_messing**3) / 3))
+x_linspace_fit_messing = (L_stab_messing_eingespannt *
+                          x_linspace**2 - ((x_linspace**3) / 3))
 params_m, covariance_m = curve_fit(D, x_messing_fit, D_x_messing)
 errors_m = np.sqrt(np.diag(covariance_m))
-print("params", *params_m)
-plt.plot(x_messing_fit, D_x_messing, 'rx', label="Messwerte")
-plt.plot(x_messing_fit, D(x_messing_fit, *params_m),
+print("Param Messing", params_m, errors_m)
+plt.plot(x_messing_fit * 1000, D_x_messing, 'rx', label="Messwerte")
+plt.plot(x_linspace_fit_messing * 1000, D(x_linspace_fit_messing, *params_m),
          'b-', label="Regressionsgrade")
 plt.xlabel(
-    r"$L\cdot x^2- \frac{x^3}{3}$")
+    r"$L\cdot x^2- \frac{x^3}{3}$/$10^{-3}\,\si{\cubic\meter}$")
 plt.ylabel(
-    r"$D(x)$")
+    r"$D(x)$/$10^{-3}\,\si{\cubic\meter}$")
+#plt.xlim(0, 53)
+#plt.ylim(0, 5.6)
+plt.axis('tight')
 plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig('Bilder/b.pdf')
-
+ascii.write([x_messing * 100, d_messing - 0.5, dx_messing - 0.5, D_x_messing], 'Messdaten/messing_einseitig.tex', format="latex",
+            names=["messpunkt x ", "D_0", "D_belastet", "D diff"])
 a_messing = ufloat(params_m[0], errors_m[0])
 E_messing = F_messing / (2 * a_messing * I_messing)
 print("E Messing", E_messing)
 
 #####################alu#################
+x_linspace = np.linspace(0, 49) / 100
 
 m_alu, d_alu, dx_alu = np.genfromtxt("Messdaten/a.txt", unpack=True)
 D_x_alu = dx_alu - d_alu
@@ -70,28 +78,33 @@ print("Dichte Alu Literatur", dichte_lit_alu)
 
 x_alu = x_alu / 100
 x_alu_fit = (L_stab_alueingespannt * x_alu**2 - ((x_alu**3) / 3))
+x_linspace_fit_alu = (L_stab_alueingespannt *
+                      x_linspace**2 - ((x_linspace**3) / 3))
 
-
-ascii.write([x_alu, d_alu, dx_alu, D_x_alu], 'Messdaten/alu_einseitig.tex', format="latex",
+ascii.write([x_alu * 100, d_alu - 1, dx_alu - 1, D_x_alu], 'Messdaten/alu_einseitig.tex', format="latex",
             names=["messpunkt x ", "D_0", "D_belastet", "D diff"])
 
 
 plt.clf()
 params, covariance = curve_fit(D, x_alu_fit, D_x_alu)
 errors = np.sqrt(np.diag(covariance))
-print("params", *params)
-plt.plot(x_alu_fit, D_x_alu, 'rx', label="Messwerte")
-plt.plot(x_alu_fit, D(x_alu_fit, *params), 'b-', label="Regressionsgrade")
+print("Param Alu", params, errors)
+plt.plot(x_alu_fit * 1000, D_x_alu, 'rx', label="Messwerte")
+plt.plot(x_linspace_fit_alu * 1000, D(x_linspace_fit_alu, *params),
+         'b-', label="Regressionsgrade")
 plt.xlabel(
-    r"$L\cdot x^2- \frac{x^3}{3}$")
+    r"$L\cdot x^2- \frac{x^3}{3}$/$10^{-3}\,\si{\cubic\meter}$")
 plt.ylabel(
-    r"$D(x)$")
+    r"$D(x)$/$10^{-3}\,\si{\cubic\meter}$")
+#plt.xlim(0, 53)
+#plt.ylim(0, 5.75)
+plt.axis('tight')
 plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig('Bilder/a.pdf')
 a_alu = ufloat(params[0], errors[0])
 F_alu = mlast_alu * g
 I_alu = d_stab_alu**4 / 12
-
+print("I alu=", I_alu)
 E_alu = F_alu / (2 * a_alu * I_alu)
 print("E alu=", E_alu)
