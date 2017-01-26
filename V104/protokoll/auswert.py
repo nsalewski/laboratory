a2a69dbea4f052aa5c239c919608dfadcaef1767
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-01-26 23:43:49 +0100

    finished b, started c and d, v is missing for c and d

--- V104/protokoll/auswert.py
+++ V104/protokoll/auswert.py
@@ -5,6 +5,7 @@ from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
 
-n, v = np.genfromtxt("Messdaten/adrianundclemens/clemensadrian_v_null.txt", unpack=True)
-
+n, v = np.genfromtxt(
+    "Messdaten/adrianundclemens/clemensadrian_v_null.txt", unpack=True)
+v = v * 5 / 4
 ascii.write([n, v], 'Messdaten/v_null_ac.tex', format='latex')
--- V104/protokoll/plot.py
+++ V104/protokoll/plot.py
@@ -8,9 +8,13 @@ import uncertainties.unumpy as unp
 # zudem: Import der ganzen daten mittels Holz-Methode, arrays für die
 # difference sind jeweils fertig und können nun gegen den gang (eventuell
 # auch gegen die bestimmte geschwindigkeit v des Wagens) geplottet werden
-
+# plathalter für die plots, hier einfach die arrays
+Gang = np.linspace(1, 10, 10)
+# für vorwärts-und rückwärtsgang einfügen
 
 # short function for generating arrays with nominalvalues
+
+
 def nomvalues_array(array):
     List = list()
     for i in range(len(array)):
@@ -19,27 +23,36 @@ def nomvalues_array(array):
     return array_noms
 
 #########
-# b)
+# b) berechnet mit unseren daten
 entartung, a, diff = np.genfromtxt("Messdaten/b.txt", unpack="True")
+
 diff = diff[1:6]
+ascii.write([diff, diff * 2], 'Messdaten/entartung_wir.tex', format='latex')
+
 diff = diff * 2  # in mm
 diff = diff * 10**(-3)
-print("Diff", diff)
 wellenlaenge = ufloat(np.mean(diff), np.std(diff, ddof=1) / np.sqrt(len(diff)))
-cadj, vnull = np.genfromtxt(
+print("Wellenlänge", wellenlaenge)
+
+messpunkt, vnull = np.genfromtxt(
     "Messdaten/unsereMessdaten_v_null.txt", unpack="True")
+ascii.write([messpunkt, vnull], 'Messdaten/v_null_wir.tex', format='latex')
+
 v_null = ufloat(np.mean(vnull), np.std(vnull, ddof=1) / np.sqrt(len(vnull)))
 c = wellenlaenge * v_null
+print("Ruhefrequenz", v_null)
 print("Schallgeschwindigkeit=", c)
 v_null_div_c = v_null / c
 eins_div_lambda = 1 / wellenlaenge
-
+print("Geforderte Größe (Gradenparameter später)=",
+      v_null_div_c, eins_div_lambda)
 ######################################
 # calculating diff
-
-Gang = np.linspace(1, 10, 10)
-
-
+#################################
+v = np.linspace(-10, 10, 20)
+# kannst du einfach löschen, wenn du die geschwindikeit des wagens für
+# Vor und zurück berechnet hast. Nenn das array am besten einfach genauso, dann musst du unten nichts ändern.
+###########################################
 a, vnull_adrianclemens = np.genfromtxt(
     "Messdaten/adrianundclemens/clemensadrian_v_null.txt", unpack="True")
 # jedesmal muss gemessene frequenz umgerechnet werden
@@ -97,6 +110,7 @@ g_quelle_rueck = [g_quelle1rueck, g_quelle2rueck, g_quelle3rueck, g_quelle4rueck
 g_quelle_vor = [g_quelle1vor, g_quelle2vor, g_quelle3vor, g_quelle4vor, g_quelle5vor,
                 g_quelle6vor, g_quelle7vor, g_quelle8vor, g_quelle9vor, g_quelle10vor]
 
+
 # for easier array-calculation
 v_null_ac_array = list()
 for i in range(10):
@@ -104,14 +118,39 @@ for i in range(10):
 v_null_ac_array = np.asarray(v_null_ac_array)
 
 # arrays ready to plot, using nominalvalue for plotting
-vquelle_diff_vor = unp.sqrt((g_quelle_vor - v_null_ac_array)**2)
-vquelle_diff_rueck = unp.sqrt((g_quelle_rueck - v_null_ac_array)**2)
-# ready to plot now
-
-#################
+vquelle_diff_vor = (g_quelle_vor - v_null_ac_array)
+vquelle_diff_rueck = (g_quelle_rueck - v_null_ac_array)
 vquelle_diff_vor_noms = nomvalues_array(vquelle_diff_vor)
 vquelle_diff_rueck_noms = nomvalues_array(vquelle_diff_rueck)
+# frag nicht, wie oft ich zwischen den datentypen springe :D
+vquelle_diff_vor_noms = vquelle_diff_vor_noms.tolist()
+vquelle_diff_rueck_noms = vquelle_diff_rueck_noms.tolist()
+vquellediff_noms = vquelle_diff_vor_noms + vquelle_diff_rueck_noms
+vquellediff_noms.sort()
 #################
+
+
+def ausgleichsgrade(v, a, b):
+    return a * v + b
+
+paramsv, covariancev = curve_fit(
+    ausgleichsgrade, v, vquellediff_noms)
+errorsv = np.sqrt(np.diag(covariancev))
+av = ufloat(paramsv[0], errorsv[0])
+bv = ufloat(paramsv[1], errorsv[1])
+print('a Messung für quelle bewegt ist in 1/Meter=', av)
+print('b = ', bv)
+plt.plot(v, vquellediff_noms, 'ro', label="Messwerte")
+plt.plot(v, ausgleichsgrade(
+    v, *paramsv), 'b-', label="Regressionsgrade")
+plt.xlabel(
+    r"Relativgeschwindigkeit der Quelle zum Empfänger $v$/$\si{\meter\per\second}$")
+plt.ylabel(
+    r"Frequenzänderung $\Delta \nu$/$\si{Hz}$")
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('Bilder/vquellebewegt.pdf')
+plt.clf()
 
 # c)part 2: difference measured by using beat-method
 delta_v = np.genfromtxt(
@@ -166,7 +205,29 @@ g10vor = ufloat(np.mean(delta_v[95:100]), np.std(
 # arrays ready to plot
 g_rueck = [g1rueck, g2rueck, g3rueck, g4rueck, g5rueck,
            g6rueck, g7rueck, g8rueck, g9rueck, g10rueck]
-
 g_vor = [g1vor, g2vor, g3vor, g4vor, g5vor, g6vor, g7vor, g8vor, g9vor, g10vor]
+
 g_rueck_noms = nomvalues_array(g_rueck)
-g_rueck_vor = nomvalues_array(g_vor)
+g_rueck_noms = -g_rueck_noms  # da wieder bezüglich des empfängers gemessen
+g_vor_noms = nomvalues_array(g_vor)
+g_rueck_noms = g_rueck_noms.tolist()
+g_vor_noms = g_vor_noms.tolist()
+g_noms = g_vor_noms + g_rueck_noms  # wie in a argumentiert, kein unterschied
+g_noms.sort()
+paramsdeltav, covariancedeltav = curve_fit(
+    ausgleichsgrade, v, g_noms)
+errorsdeltav = np.sqrt(np.diag(covariancedeltav))
+adeltav = ufloat(paramsdeltav[0], errorsdeltav[0])
+bdeltav = ufloat(paramsdeltav[1], errorsdeltav[1])
+print('a direkte Messung über Schwebung in 1/Meter=', adeltav)
+print('b = ', bdeltav)
+plt.plot(v, g_noms, 'ro', label="Messwerte")
+plt.plot(v, ausgleichsgrade(
+    v, *paramsdeltav), 'b-', label="Regressionsgrade")
+plt.xlabel(
+    r"Geschwindigkeit des Reflektors $v_{\mathrm{R}}$/$\si{\meter\per\second}$")
+plt.ylabel(
+    r"Frequenzänderung $\Delta \nu$/$\si{Hz}$")
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('Bilder/schwebung.pdf')
