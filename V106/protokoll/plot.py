160579ebcf693b3d7c2023f554a0918c08d97a52
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-01-31 18:12:15 +0100

    added messdaten, included graphics in theory (well dat was horrific) and finished theory

--- V106/protokoll/plot.py
+++ V106/protokoll/plot.py
@@ -4,16 +4,16 @@ from scipy.optimize import curve_fit
 from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
-
-ascii.write([diff, diff * 2], 'Messdaten/entartung_wir.tex', format='latex')
-
-plt.plot(v, g_noms, 'ro', label="Messwerte")
-plt.plot(v, ausgleichsgrade(
-    v, *paramsdeltav), 'b-', label="Regressionsgrade")
-plt.xlabel(
-    r"Geschwindigkeit des Reflektors $v_{\mathrm{R}}$/$\si{\meter\per\second}$")
-plt.ylabel(
-    r"Frequenzänderung $\Delta \nu$/$\si{Hz}$")
-plt.legend(loc='best')
-plt.tight_layout()
-plt.savefig('Bilder/schwebung.pdf')
+#
+#ascii.write([diff, diff * 2], 'Messdaten/entartung_wir.tex', format='latex')
+#
+#plt.plot(v, g_noms, 'ro', label="Messwerte")
+# plt.plot(v, ausgleichsgrade(
+#    v, *paramsdeltav), 'b-', label="Regressionsgrade")
+# plt.xlabel(
+#    r"Geschwindigkeit des Reflektors $v_{\mathrm{R}}$/$\si{\meter\per\second}$")
+# plt.ylabel(
+#    r"Frequenzänderung $\Delta \nu$/$\si{Hz}$")
+# plt.legend(loc='best')
+# plt.tight_layout()
+# plt.savefig('Bilder/schwebung.pdf')
