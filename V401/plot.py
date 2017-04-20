6d3c1b8d245e8cb163180d08955615eabbee8c25
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-04-20 13:16:12 +0200

    added aufbau, durchführung, started auswertung

--- V401/plot.py
+++ V401/plot.py
@@ -4,3 +4,14 @@ from scipy.optimize import curve_fit
 from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
+d, n = np.genfromtxt("Messdaten/spiegelverschiebung.txt", unpack=True)
+uebersetzung=5.024
+d=d/100
+d=d/uebersetzung
+wellenlaenge=(2*d)/n
+
+ascii.write([np.round(d*10000,2),n, np.round(wellenlaenge * 10**9 , 2)], 'Messdaten/tab_wellenlaenge.tex', format="latex",
+            names=['d','n','wellennlänge in nanometerz'])
+
+wellen=ufloat(np.mean(wellenlaenge), np.std(wellenlaenge))
+print("Wellenlänge: ", wellen)
