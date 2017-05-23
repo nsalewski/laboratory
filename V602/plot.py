0c51c496276dda1bd420c0175625c69b0c619451
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-05-23 16:55:20 +0200

    daata

--- V602/plot.py
+++ V602/plot.py
@@ -4,19 +4,26 @@ from scipy.optimize import curve_fit
 from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
-Z=[30,32,35,37,38,40,41]
+Z=[29,29,29,30,32,35,37,38,40,41,79,79]
 Z=np.asarray(Z)
-E=[9.65,11.1031,13.4737,15.1997,16.1046,17.9976,18.9856]
+E=[8.048,8.028,8.905,9.65,11.1031,13.4737,15.1997,16.1046,17.9976,18.9856,13.739,11.925]
 E=np.asarray(E)
-r=13.6
-sigma=Z-np.sqrt(E/r)
-print(sigma)
+E=E*1000
+print(E)
+enull=1.6021766208*10**(-19)
+R=13.6
+r=10973731.568508
 c=299792458
 d=201.4*10**(-12)
 h=ufloat(6.626070040*10**(-34),0.000000081*10**(-34))
-
-theta=np.arcsin(c*h.nominal_value/2*d*E)
-
+Rzwo=r*h.nominal_value*c
+R=Rzwo
+E=E*enull
+print(Z[0]-np.sqrt(E[0]/R))
+theta=(360/(2*np.pi))*(np.arcsin(((c*h.nominal_value)/(2*d*E))))
+sigma=Z-np.sqrt(E/R)
+print(sigma)
+print(theta)
 
 #n,f=np.genfromtxt("Messdaten/b_2.txt",unpack=True)
 #f=f*1000
