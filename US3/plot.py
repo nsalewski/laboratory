ce3135eb64dc800ee64d72cc2f99d7537ea3cbfa
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-06-08 00:30:14 +0200

    started auswertung teil 2; eindringtiefe strömungsprofil

--- US3/plot.py
+++ US3/plot.py
@@ -6,6 +6,40 @@ from uncertainties import ufloat
 import uncertainties.unumpy as unp
 
 
+
+
+
+
+
+########################################################
+#Strömungsprofil
+
+def local_plot(fname,pname):
+    t,I,f=np.genfromtxt("Messdaten/b_{}.txt".format(fname),unpack=True)
+    t=t*3/2
+    t_ls=np.linspace(np.min(t)-0.1*(np.max(t)-np.min(t)),np.max(t)+0.1*(np.max(t)-np.min(t)))
+    plt.plot(t, I, 'rx', label="Messwerte für P={}\%".format(pname))
+    plt.ylabel(r"$I_\mathrm{S}$/$\si{\square\volt\per\second}$")
+    plt.xlabel(r"$x/\si{\milli\meter}$")
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.savefig('Bilder/I{}.pdf'.format(fname))
+    plt.clf()
+
+    plt.plot(t, f, 'rx', label="Messwerte für P={}\%".format(pname))
+    plt.ylabel(r"$\Delta \nu$/$\si{\Hz}$")
+    plt.xlabel(r"$x/\si{\milli\meter}$")
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.savefig('Bilder/f{}.pdf'.format(fname))
+    plt.clf()
+    ascii.write([t,I,f],'Messdaten/tab_{}'.format(fname),format='latex',names=[r"Eindringtiefe/$\si{\milli\meter}$",r"$I_\mathrm{S}$/$\si{\square\volt\per\second}$",r"$\Delta \nu$/$\si{\Hz}$"])
+local_plot(70,70)
+local_plot('45-2','45.2')
+
+########################################################
+
+
 #n,f=np.genfromtxt("Messdaten/b_2.txt",unpack=True)
 #f=f*1000
 #theta=(n*np.pi)/14
@@ -37,4 +71,3 @@ import uncertainties.unumpy as unp
 #params, covariance = curve_fit(theorie,Z,np.sqrt(Ek))
 #errors = np.sqrt(np.diag(covariance))
 #ryd=ufloat(params[0],errors[0])
-
