0112d89acbec9429e1d24775586ec5476874cf42
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-06-22 15:43:29 +0200

    started theorie

--- V702/plot.py
+++ V702/plot.py
@@ -4,6 +4,70 @@ from scipy.optimize import curve_fit
 from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
+nulleffekt=220
+tnull=900
+nulleff=220/900
+def theorie(x,m,b):
+    return m*x+b
+
+
+########Auswertung Silber
+imps=np.genfromtxt("Messdaten/silver.txt", unpack=True)
+imps=imps[:52]
+t_int=8
+nullsilver=nulleff*t_int
+imps=imps-nullsilver
+t=np.linspace(0,len(imps),len(imps))
+print(nullsilver)
+#es werden werte aus dem array entfernt, welche unter 1+nullsilver liegen, sonst probleme im logarithmus
+
+def plot(teins,tzwei):
+    arrteins=imps[0:teins]
+    lineins=t[0:teins]
+    arrtzwei=imps[tzwei:len(imps)]
+    linzwei=t[tzwei:len(imps)]
+    params2lin, covariance2lin = curve_fit(theorie,8*linzwei,(arrtzwei))
+
+    arrtzwei=np.log(arrtzwei)
+    print(arrtzwei)
+    params2, covariance2 = curve_fit(theorie,8*linzwei,(arrtzwei))
+    errors2 = np.sqrt(np.diag(covariance2))
+    m2=ufloat(params2[0],errors2[0])
+    b2=ufloat(params2[1],errors2[1])
+    print(r"Zweiter Prozess ")
+    print("m",m2,r'\n b',b2)
+
+    abzug=theorie(lineins,*params2lin)
+    arrteins=arrteins-abzug
+    arrteins=np.log(arrteins)
+    params1, covariance1 = curve_fit(theorie,8*lineins,(arrteins))
+    errors1 = np.sqrt(np.diag(covariance1))
+    m1=ufloat(params1[0],errors1[0])
+
+    b1=ufloat(params1[1],errors1[1])
+    print(r"Erster Prozess ")
+    print("m",m1,r'\n b',b1)
+    linspace1=np.linspace(0,teins)
+
+    plt.plot(linspace1*8, theorie(linspace1*8, *params1), 'g-', label="Ausgleichsgrade 1")
+    a=np.log(imps)-np.log(imps-np.sqrt(imps))
+    b=(np.log(imps+np.sqrt(imps))-np.log(imps))
+    for x in range(0,len(t)):
+        if ((np.log(imps[x])-a[x])<=0.0):
+            a[x]=np.log(imps[x])
+    plt.errorbar(t*8, np.log(imps), yerr=[a,b], xerr=None, fmt='x', label="Messwerte")
+    #plt.plot(t*8, np.log(imps), 'rx', label="Messwerte")
+    linspace2=np.linspace(tzwei,54)
+    plt.plot(linspace2*8, theorie(linspace2*8,*params2),'b-', label="Ausgleichsgrade 2")
+    plt.axvline(x=teins*8, ls=':', color="g")
+    plt.axvline(x=tzwei*8, ls=':', color="c")
+    plt.ylabel(r"$\log(N(t))$/$\si{\per\second}$")
+    plt.xlabel(r"$t$/$\si{\second}$")
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.savefig('Messdaten/silber.pdf')
+
+plot(11,23)
 
 
 #n,f=np.genfromtxt("Messdaten/b_2.txt",unpack=True)
@@ -37,4 +101,3 @@ import uncertainties.unumpy as unp
 #params, covariance = curve_fit(theorie,Z,np.sqrt(Ek))
 #errors = np.sqrt(np.diag(covariance))
 #ryd=ufloat(params[0],errors[0])
-
