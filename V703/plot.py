6abac63768a5943ee9d7ca169e07172656faa846
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2017-07-01 21:18:33 +0200

    started auswertung

--- V703/plot.py
+++ V703/plot.py
@@ -5,7 +5,34 @@ from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
 
+def theorie(x,m,b):
+    return m*x+b
 
+impsroh, u, i=np.genfromtxt('Messdaten/a.txt',unpack=True)
+imps=impsroh/60
+impsreg=imps[4:len(imps)-3]
+print(impsreg)
+ascii.write([u,impsroh,np.round(imps,2)],'Messdaten/tab_a.tex',format='latex',names=["U in V","Imps mess","Imps per second"])
+params, covariance = curve_fit(theorie,u[4:len(imps)-3], impsreg)
+errors = np.sqrt(np.diag(covariance))
+m=ufloat(params[0],errors[0])
+b=ufloat(params[1], errors[1])
+print('Messung a: ')
+print("m= ", m)
+print("b= ", b)
+uls=np.linspace(min(u)-0.1*min(u),max(u)+0.1*max(u))
+plt.plot(uls,theorie(uls,*params), 'b-', label="Ausgleichsgrade")
+plt.axvline(x=480, ls=':', color="k", label="Arbeitsbereich des Zählrohrs")
+plt.axvline(x=max(u)-30, ls=':', color="k")
+
+plt.errorbar(u,imps, yerr=[-np.sqrt(imps),+np.sqrt(imps)], xerr=None, fmt='rx', label="Messwerte")
+plt.ylabel(r"$N$/$\frac{\mathrm{[Imps]}}{\si{\second}}$")
+plt.xlabel(r"$U$/$\si{\volt}$")
+plt.xlim(min(u)-0.1*min(u),max(u)+0.1*max(u))
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('Bilder/a.pdf')
+print('*****************************************')
 #n,f=np.genfromtxt("Messdaten/b_2.txt",unpack=True)
 #f=f*1000
 #theta=(n*np.pi)/14
@@ -37,4 +64,3 @@ import uncertainties.unumpy as unp
 #params, covariance = curve_fit(theorie,Z,np.sqrt(Ek))
 #errors = np.sqrt(np.diag(covariance))
 #ryd=ufloat(params[0],errors[0])
-
