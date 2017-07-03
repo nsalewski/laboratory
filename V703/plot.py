b3beeb62d867c5da30da3e3a20309db953f7b323
Niko Salewski <niko.salewski@tu-dortmund.de>
2017-07-03 14:01:38 +0200

    pommes

--- V703/plot.py
+++ V703/plot.py
@@ -8,26 +8,26 @@ import uncertainties.unumpy as unp
 def theorie(x,m,b):
     return m*x+b
 
-impsroh, u, i=np.genfromtxt('Messdaten/a.txt',unpack=True)
+impsroh, uA, otto =np.genfromtxt('Messdaten/a.txt',unpack=True)
 imps=impsroh/60
 impsreg=imps[4:len(imps)-3]
-ascii.write([u,impsroh,np.round(imps,2),np.round((np.sqrt(imps)/imps),3)],'Messdaten/tab_a.tex',format='latex',names=["U in V","Imps mess","Imps per second","err"])
-params, covariance = curve_fit(theorie,u[4:len(imps)-3], impsreg)
+ascii.write([uA,impsroh,np.round(imps,2),np.round((np.sqrt(imps)/imps),3)],'Messdaten/tab_a.tex',format='latex',names=["U in V","Imps mess","Imps per second","err"])
+params, covariance = curve_fit(theorie,uA[4:len(imps)-3], impsreg)
 errors = np.sqrt(np.diag(covariance))
 m=ufloat(params[0],errors[0])
 b=ufloat(params[1], errors[1])
 print('Messung a: ')
 print("m= ", m)
 print("b= ", b)
-uls=np.linspace(min(u)-0.1*min(u),max(u)+0.1*max(u))
+uls=np.linspace(min(uA)-0.1*min(uA),max(uA)+0.1*max(uA))
 plt.plot(uls,theorie(uls,*params), 'b-', label="Ausgleichsgrade")
 plt.axvline(x=480, ls=':', color="k", label="Arbeitsbereich des Zählrohrs")
-plt.axvline(x=max(u)-30, ls=':', color="k")
+plt.axvline(x=max(uA)-30, ls=':', color="k")
 
-plt.errorbar(u,imps, yerr=[-np.sqrt(imps)/imps,+np.sqrt(imps)/imps], xerr=None, fmt='rx', label="Messwerte")
+plt.errorbar(uA,imps, yerr=[-np.sqrt(imps)/imps,+np.sqrt(imps)/imps], xerr=None, fmt='rx', label="Messwerte")
 plt.ylabel(r"$N$/$\frac{\mathrm{[Imps]}}{\si{\second}}$")
 plt.xlabel(r"$U$/$\si{\volt}$")
-plt.xlim(min(u)-0.1*min(u),max(u)+0.1*max(u))
+plt.xlim(min(uA)-0.1*min(uA),max(uA)+0.1*max(uA))
 plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig('Bilder/a.pdf')
@@ -68,6 +68,21 @@ T=(imps1+imps3-imps2)/(2*imps1*imps3)
 T=T*10**6
 print('Totzeit=',T)
 
+########################################################################################
+ireg=otto[4:len(otto)-3]
+ureg = uA[4:len(uA)-3]
+impsroh2 = impsroh[4:len(impsroh)-3]
+Q = ireg/impsreg
+Qe = Q/(1.6021766208*10**(-19))
+print(len(Qe), len(ureg), len(impsroh2), len(impsreg))
+ascii.write([ureg, impsroh2, impsreg, np.round(Q,3), np.round(Qe,3)],'Messdaten/tab_d.tex',format='latex')
+lol = sum(Qe)/len(Qe)
+print(lol)
+
+
+
+
+########################################################################################
 #n,f=np.genfromtxt("Messdaten/b_2.txt",unpack=True)
 #f=f*1000
 #theta=(n*np.pi)/14
