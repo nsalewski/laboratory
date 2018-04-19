8b0bd00d913b65a0dc2a54b9f39289e0742283e5
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2018-04-19 07:20:21 +0200

    needed to fix some Unicode_Errors, which are caused by python version 2.7.14 and matplotlib (matplotlibrc contains non-ascii characters), there is another problem currently with module installation paths. I will try to fix it clean asap, but current dirty fix runs like a charm

--- FP/V21_optical_pumping/plot.py
+++ FP/V21_optical_pumping/plot.py
@@ -1,17 +1,14 @@
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8') 
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 from astropy.io import ascii
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
-from modules.table.textable import latex_tab
+import textable
 
 arr1=[1,2,3,4]
 arr2=[43,213,123,12]
-latex_tab(data=[arr1,arr2],names=["bla","blup"], filename="latex_tables/example.tex",caption="Caption",label="test")
+textable.latex_tab(data=[arr1,arr2],names=["bla","blup"], filename="latex_tables/example.tex",caption="Caption",label="test")
 
 
 
