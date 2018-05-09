4bdddfdf020d24def8a06e00a68a803bea7fc4e1
Niko Salewski <niko.salewski@tu-dortmund.de>
2018-05-09 18:59:59 +0200

    mittwoch abend

--- FP/V27_Zeeman_effect/plot.py
+++ FP/V27_Zeeman_effect/plot.py
@@ -10,6 +10,14 @@ from modules.table import textable
 import scipy.constants as const
 import math as math
 
+#Konstanten
+
+h = 6.626070040 *10**(-34) #Plancksche Wirkungsquantum
+c = 299792458#Lichtgeschw
+mu_B = 927.4009994 * 10**(-26) #Bohrsches Magneton
+rot_lambda_D = 48.913 * 10**(-12)
+blau_lambda_D = 26.952 * 10**(-12)
+
 ##############################################################################################
 # Eichen
 I, B = np.genfromtxt('data/magnetfeld.txt', unpack=True)
@@ -28,12 +36,29 @@ plt.ylabel(r'Magnetfeld $B / \si{\milli\tesla}$')
 plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig('pictures/eichi.pdf')
-
-
-
-
+plt.clf()
 
 ##############################################################################################
+
+lambd = 643.8*10**(-9)
+B_rot = 652*10**(-3)
+Ds = [148, 136, 130, 132, 132, 126, 120, 122, 122, 120]
+ds = [72, 72, 68, 66, 70, 70, 66, 62, 62, 66]
+
+D_s_mittel = ufloat(np.mean(Ds),np.std(Ds,ddof=1)/np.sqrt(len(Ds)))
+d_s_mittel = ufloat(np.mean(ds),np.std(ds,ddof=1)/np.sqrt(len(ds)))
+print('D_s Mittelwert: ', D_s_mittel)
+print('d_s Mittelwert: ', d_s_mittel)
+delta_lambda = 0.5 * d_s_mittel/D_s_mittel * rot_lambda_D
+print('delta_lambda: ', delta_lambda)
+g = (h*c* delta_lambda) / (lambd**2*mu_B*B_rot)
+print('g: ', g)
+
+
+
+
+
+
 
 #Daten
 #rf,horizontal_1,horizontal_2, peak_1,peak_2=np.genfromtxt("data/data.txt",unpack=True)
