782890d906914b7f4fca6f99815506485bb5e9d2
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2018-06-02 15:31:59 +0200

    started auswertung

---
+++ FP/V46_faraday_effect/modules/table/textable.py
@@ -0,0 +1,59 @@
+import uncertainties
+def latex_tab(data=[[1,2,3],[42,42,42]],names=["col1","col2"],filename="test.tex",caption="Caption",label="test", dec_points=[0,2],tableformat=3.3):
+    try:
+        for i in range(len(data)-1):
+            if not(len(data[i])==len(data[i+1])):
+                raise TypeError("Arrays must have same dimension!"+" Error occured in comparison of data["+str(i)+"]"+"and data["+str(i+1)+"]")
+
+
+
+        if not(len(names)==len(data) and len(dec_points)==len(data)):
+            raise TypeError("data and names and dec_points must have same dimension! "+"len(data)= "+str(len(data))+"; len(names)= "+str(len(names)) +"; len(dec_points)= "+str(len(dec_points)))
+    except TypeError: raise
+    else:
+        texfile = open(filename,"w")
+        texfile.write("\\begin{table}\n");
+        texfile.write(" \\caption{"+caption+"}\n");
+        texfile.write(" \\label{tab:"+label+"}\n")
+        texfile.write(" \\centering\n")
+        texfile.write("\\sisetup{table-format=%s}"%tableformat)
+        texfile.write(" \\begin{tabular}{")
+        for col in data:
+            texfile.write("S")
+        texfile.write("}\n");
+        texfile.write(" \\toprule \n    ");
+        for i in range(len(names)-1):
+            texfile.write("{"+names[i]+"}& ")
+        texfile.write("{"+names[len(names)-1]+"}")
+        texfile.write(" \\\\\n");
+        texfile.write("     \\midrule\n");
+        for i in range(len(data[0])):
+            texfile.write("     ")
+            for j in range(len(data)-1):
+
+                if isinstance(data[j][i],uncertainties.core.Variable):
+                    data[j][i]=(str(data[j][i])).replace('+/-',' \pm ')
+                    texfile.write('$\\num{'+ data[j][i]+'}$')
+                else:
+                    texfile.write(("{:10.%df}"%dec_points[j]).format(data[j][i])+" & ")
+            if isinstance(data[len(data)-1][i],uncertainties.core.Variable):
+                data[len(data)-1][i]=(str(data[len(data)-1][i])).replace('+/-','\pm')
+                texfile.write('$\\num{'+ data[len(data)-1][i]+'}$')
+            else:
+                texfile.write(("{:10.%df}"%dec_points[len(dec_points)-1]).format(data[len(data)-1][i]))
+            texfile.write(" \\\\\n")
+        texfile.write(" \\bottomrule\n");
+        texfile.write(" \\end{tabular}\n");
+        texfile.write("\\end{table}");
+        texfile.close()
+
+
+
+
+if __name__ == "__main__":
+    arr1=[1,2,3,4,5]
+    arr2=[0.8,12,234,42,1.28]
+    import textable
+    print("42 is the answer to life the universe and everything!")
+    print("Running this module as script generates a sample table.tex")
+    latex_tab([arr1,arr2],["col1","col2"],"table.tex",'caption','sample')
---
+++ FP/V46_faraday_effect/plot.py
@@ -0,0 +1,141 @@
+#!usr/bin/env python3
+#coding:utf8
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from astropy.io import ascii
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+from modules.table import textable
+import scipy.constants as const
+import math as math
+#arr1=[0.4,0.75,1.4]
+#arr2=[2,3,4]
+#textable.latex_tab(data=[arr1,arr2],names=[r"title column 1",r"title column 2"], filename=r"example.tex",caption=r"Beautiful caption",label=r"important_label",dec_points=[2,0])
+def manipulate(arr):
+    for elem in range(len(arr)):
+        if arr[elem-1]<180:
+            arr[elem-1]=arr[elem-1]+180
+        else: arr[elem-1]=arr[elem-1]-180
+    return arr
+def theorie(x,a,mu,b):
+    return ((a*np.exp(-((x-mu)**2)/(b))))
+def winkel(grad,sec):
+    sec=sec*1/60
+    grad=grad+sec
+    return grad
+def lin(x,a):
+    return a*x
+def eff_mass(a,B,N):
+    return np.sqrt((e0)**3*N*B)/(8*np.pi**2*eps*c**3*n*a)
+#daten importieren
+b,z=np.genfromtxt("data/b_feld.txt",unpack=True)
+
+f1,d1_hin,d1_hins,d1_rueck,d1_ruecks=np.genfromtxt("data/1_probe.txt",unpack=True)
+f2,d2_hin,d2_hins,d2_rueck,d2_ruecks=np.genfromtxt("data/2_probe.txt",unpack=True)
+f3,d3_hin,d3_hins,d3_rueck,d3_ruecks=np.genfromtxt("data/3_probe.txt",unpack=True)
+f1=f1*10**(-6)
+f2=f2*10**(-6)
+f3=f3*10**(-6)
+l1=1.296*10**(-3)
+l2=1.36*10**(-3)
+l3=5.11*10**(-3)
+
+#bogensekunden addieren
+grad1_hin=winkel(d1_hin,d1_hins)
+grad1_rueck=winkel(d1_rueck,d1_ruecks)
+grad2_hin=winkel(d2_hin,d2_hins)
+grad2_rueck=winkel(d2_rueck,d2_ruecks)
+grad3_hin=winkel(d3_hin,d3_hins)
+grad3_rueck=winkel(d3_rueck,d3_ruecks)
+
+#umrechnen auf gleichen Bezugspunkt
+grad1_hin=manipulate(grad1_hin)
+grad1_rueck=manipulate(grad1_rueck)
+grad2_hin=manipulate(grad2_hin)
+grad2_rueck=manipulate(grad2_rueck)
+grad3_hin=manipulate(grad3_hin)
+grad3_rueck=manipulate(grad3_rueck)
+
+grad1=(1/(2*l1)*(grad1_rueck-grad1_hin)*2*np.pi/360)
+grad2=(1/(2*l2)*(grad2_rueck-grad2_hin)*2*np.pi/360)
+grad3=(1/(2*l3)*(grad3_rueck-grad3_hin)*2*np.pi/360)
+#Berechnung delta theta
+delta1=grad1-grad3
+delta2=grad2-grad3
+textable.latex_tab(data=[f1,grad3,grad1,grad2,delta1,delta2],names=[r"$\lambda$/$\si{\micro\meter}$",r"$\theta_{\mathrm{und}}$/$\si{\radian\per\meter}$",r"$\theta_{\mathrm{d1}}$/$\si{\radian\per\meter}$",r"$\theta_{\mathrm{d2}}$/$\si{\radian\per\meter}$",r"$\Delta \theta_{\mathrm{d1}}$/$\si{\radian\per\meter}$",r"$\Delta \theta_{\mathrm{d2}}$/$\si{\radian\per\meter}$"], filename=r"tables/eff_mass.tex",caption=r"Werte der $\Delta \theta$ zwischen undotiertem und dotiertem $\ce{GaAs}$ zur Bestimmung der effektiven Masse der Kristallelektronen",label=r"eff_mass",dec_points=[2,2,2,2,2,2],tableformat=4.2)
+
+
+
+#Tabellen theta
+textable.latex_tab(data=[f1*10**6,grad1_hin,grad1_rueck,grad1],names=[r"$\lambda$/$\si{\micro\meter}$",r"$\theta_1$/$\si{\degree}$",r"$\theta_2$/$\si{\degree}$",r"$|\theta|$/$\si{\radian\per\meter}$"], filename=r"tables/probe1.tex",caption=r"Messwerte der Faraday-Rotation für die dotierte Probe $\ce{GaAs}_{d1}$",label=r"probe1",dec_points=[2,2,2,2],tableformat=4.2)
+textable.latex_tab(data=[f2*10**6,grad2_hin,grad2_rueck,grad2],names=[r"$\lambda$/$\si{\micro\meter}$",r"$\theta_1$/$\si{\degree}$",r"$\theta_2$/$\si{\degree}$",r"$|\theta|$/$\si{\radian\per\meter}$"], filename=r"tables/probe2.tex",caption=r"Messwerte der Faraday-Rotation für die dotierte Probe $\ce{GaAs}_{d2}$",label=r"probe2",dec_points=[2,2,2,2],tableformat=4.2)
+textable.latex_tab(data=[f3*10**6,grad3_hin,grad3_rueck,grad3],names=[r"$\lambda$/$\si{\micro\meter}$",r"$\theta_1$/$\si{\degree}$",r"$\theta_2$/$\si{\degree}$",r"$|\theta|$/$\si{\radian\per\meter}$"], filename=r"tables/probe3.tex",caption=r"Messwerte der Faraday-Rotation für die undotierte Probe $\ce{GaAs}_{und}$",label=r"probe3",dec_points=[2,2,2,2],tableformat=4.2)
+#Tabelle Magnetfeld
+textable.latex_tab(data=[z,b],names=[r"$z$/$\si{\centi\meter}$",r"$B$/$\si{\milli\tesla}$"], filename=r"tables/magnetfeld.tex",caption=r"Messung des Magnetfelds in Abhängigkeit zum Ort $z$ (Probe ist etwa bei $\SI{3.1}{\centi\meter}$ platziert)",label=r"magnetfeld",dec_points=[2,0],tableformat=3.2)
+
+
+z_theo=np.linspace(0,6,50)
+#Ausgleichsrechnung Magnetfeld
+params, covariance = curve_fit(theorie,z,b)
+errors = np.sqrt(np.diag(covariance))
+
+print("Erwartungswert",params[1],errors[1])
+#lin regress delta
+paramsd1, covarianced1 = curve_fit(lin,f1**2,delta1)
+errorsd1 = np.sqrt(np.diag(covarianced1))
+paramsd2, covarianced2 = curve_fit(lin,f1**2,delta2)
+errorsd2 = np.sqrt(np.diag(covarianced2))
+a1=ufloat(paramsd1[0],errorsd1[0])
+a2=ufloat(paramsd2[0],errorsd2[0])
+n=3.3
+e0=const.e
+eps=const.epsilon_0
+c=const.c
+B=377.5*10**(-4)
+print("Delta_1 Steigung", a1)
+print("Delta_2 Steigung", a2)
+print("Effektive Masse 1",eff_mass(a1,B,2.8*10**18)/const.m_e)
+print("Effektive Masse 2",eff_mass(a2,B,1.2*10**18)/const.m_e)
+
+#Plot Magnetfeld
+plt.plot((params[1],params[1]),(0,400), 'r--', label=r"Erwartungswert der Normalverteilung")
+plt.plot(z,b, 'ro', label="Messwerte $B$")
+plt.ylabel(r"$B/\si{\milli\tesla}$")
+plt.xlabel(r"z/\si{\centi\meter}")
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('pictures/B_feld.pdf')
+plt.clf()
+
+#Plot theta
+plt.plot(f1*10**6,abs(grad1), 'ro', label=r"Messwerte $\theta_{\mathrm{d1}}$")
+plt.plot(f2*10**6,abs(grad2), 'go', label=r"Messwerte $\theta_{\mathrm{d2}}$")
+plt.plot(f3*10**6,abs(grad3), 'bo', label=r"Messwerte $\theta_{\mathrm{und}}$")
+plt.ylabel(r"$\theta$/$\si{\radian\per\meter}")
+plt.xlabel(r"$\lambda$/$\si{\micro\meter}$")
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('pictures/winkel_gg_wellenlaenge.pdf')
+plt.clf()
+
+
+
+#plot delta
+plt.plot(f1**2,delta1, 'ro', label=r"$\Delta \theta_{\mathrm{d1}}$")
+plt.plot(f1**2,lin(f1**2,*paramsd1), 'b-', label="Ausgleichsgrade")
+plt.ylabel(r"$\Delta \theta_{\mathrm{d1}}$/$\si{\radian\per\meter}$")
+plt.xlabel(r"$\lambda^{2}$/$\si{\square\meter}$")
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('pictures/delta1.pdf')
+plt.clf()
+plt.plot(f1**2,delta2, 'ro', label=r"$\Delta \theta_{\mathrm{d2}}$")
+plt.plot(f1**2,lin(f1**2,*paramsd2), 'b-', label="Ausgleichsgrade")
+plt.ylabel(r"$\Delta \theta_{\mathrm{d2}}$/$\si{\radian\per\meter}$")
+plt.xlabel(r"$\lambda^{2}$/$\si{\square\meter}$")
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('pictures/delta2.pdf')
+plt.clf()
